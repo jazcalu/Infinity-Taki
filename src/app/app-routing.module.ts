34213103e3218bda13fee3fd5d76db5ac5dc9a27
Luis Ccalluchi Lopez <daprimovaria@outlook.com>
2019-06-02 11:11:12 -0500

    Sidenav con animacion, falta cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { NavbarComponent } from './components/header/navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'sidenav',
+    component: NavbarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
