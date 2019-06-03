d4642141df7f0cae6f3e6d833bfb5f5e03388ac3
Luis Ccalluchi Lopez <daprimovaria@outlook.com>
2019-06-02 22:11:49 -0500

    Creacion del main

--- src/app/components/header/login/login.component.ts
+++ src/app/components/header/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
+  modalFormDarkEmail = new FormControl('', Validators.email);
+  modalFormDarkPassword = new FormControl('', Validators.required);
 
   constructor() { }
 
--- src/app/components/main-page/new-music/new-music.component.ts
+++ src/app/components/main-page/new-music/new-music.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-new-music',
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewMusicComponent implements OnInit {
 
-  constructor() { }
+  cards = [
+    { img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg' },
+    { img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg' },
+    { img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg' },
+    { img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg' },
+    { img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg' },
+    { img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg' },
+    { img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg' },
+    { img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg' },
+    { img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg' },
+  ];
 
-  ngOnInit() {
+  slides: any = [[]];
+
+  constructor(private renderer: Renderer2) { }
+
+
+  chunk(arr, chunkSize) {
+    let R = [];
+    for (let i = 0, len = arr.length; i < len; i += chunkSize) {
+      R.push(arr.slice(i, i + chunkSize));
+    }
+    return R;
   }
 
+  ngOnInit() {
+    this.slides = this.chunk(this.cards, 3);
+  }
+
+// tslint:disable-next-line: use-life-cycle-interface
+  ngAfterViewInit() {
+    const buttons = document.querySelectorAll('.btn-floating');
+    buttons.forEach((el: any) => {
+      this.renderer.removeClass(el, 'btn-floating');
+      this.renderer.addClass(el, 'px-3');
+      this.renderer.addClass(el.firstElementChild, 'fa-3x');
+    });
+  }
 }
