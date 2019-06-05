eb98ce432cb1ed801edf0194c8d7fd1c11e882dd
Luis Ccalluchi Lopez <daprimovaria@outlook.com>
2019-06-05 11:33:04 -0500

    Back Ready GET y POST

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,39 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { NavbarComponent } from './components/header/navbar/navbar.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { MusicContentComponent } from './components/music-content/music-content.component';
+import { UserContentComponent } from './components/user-content/user-content.component';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
   {
     path: 'main',
     component: MainPageComponent
-  }
+  },
+  {
+    path: 'perfil',
+    component: ProfileComponent
+  },
+  {
+    path: 'tops',
+    component: MusicContentComponent
+  },
+  {
+    path: 'news',
+    component: UserContentComponent
+  },
+  {
+    path: 'upload',
+    component: FileUploadComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+
+
 ];
 
 @NgModule({
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  ObjUsuario = {
+    usu_nom: '',
+    usu_ape: '',
+    usu_nomArt: '',
+    usu_email: '',
+    usu_pass: ''
+  }
+
+  URL = 'http://172.23.11.119:3000/api/usuario/create';
+  // URL = 'http://172.23.11.119:3000/api/usuario';
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
+  createUser() {
+
+    this.http.post(this.URL,this.ObjUsuario)
+      .subscribe(data =>{
+        console.log(data)
+      }, error => {
+        console.log(error)
+      })
+  }
 }
