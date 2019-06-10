5c317067bbbc0f7126f646564752eea3dc47fdc2
Luis Ccalluchi Lopez <daprimovaria@outlook.com>
2019-06-10 15:07:21 -0500

    Cambios minimos

--- src/app/components/header/login/login.component.ts
+++ src/app/components/header/login/login.component.ts
@@ -28,10 +28,10 @@ export class LoginComponent implements OnInit {
   onLogin() {
     this.authService.LogingUser(this.ObjUsuario).subscribe(respuesta => {
         console.log(respuesta);
-        return true
+        return true;
       },error => {
-        console.log(error)
-        return false
+        console.log(error);
+        return false;
       })
   }
 }
